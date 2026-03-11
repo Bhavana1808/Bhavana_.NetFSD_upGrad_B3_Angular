e9014379c35e63777aee35a498132fcc5d7d95f4
Bhavana1808 <yanalabhavana@gmail.com>
2026-03-11 17:18:56 +0530

    Add files via upload

---
+++ Week_4/Day_18/student grade evaluator.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                string name;
+                int marks;
+
+                Console.Write("Enter Name: ");
+                name = Console.ReadLine();
+
+                Console.Write("Enter Marks: ");
+                marks = int.Parse(Console.ReadLine());
+
+                if (marks < 0 || marks > 100)
+                {
+                    Console.WriteLine("Invalid Marks");
+                }
+                else if (marks >= 80)
+                {
+                    Console.WriteLine("Student: " + name);
+                    Console.WriteLine("Grade: A");
+                }
+                else if (marks >= 60)
+                {
+                    Console.WriteLine("Student: " + name);
+                    Console.WriteLine("Grade: B");
+                }
+                else if (marks >= 50)
+                {
+                    Console.WriteLine("Student: " + name);
+                    Console.WriteLine("Grade: C");
+                }
+                else if (marks >= 40)
+                {
+                    Console.WriteLine("Student: " + name);
+                    Console.WriteLine("Grade: D");
+                }
+                else
+                {
+                    Console.WriteLine("Student: " + name);
+                    Console.WriteLine("Grade: Fail");
+                }
+            }
+        }
+       
+        }
+    
