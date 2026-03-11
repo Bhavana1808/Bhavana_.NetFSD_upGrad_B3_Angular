2c58e85f744ebfb9f8cbd50701c8d6698ea50f75
Bhavana1808 <yanalabhavana@gmail.com>
2026-03-11 17:21:52 +0530

    Add files via upload

---
+++ Week_4/Day_18/Bonus_calculator.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string name;
+            double salary;
+            double bonus;
+            double finalSalary;
+            int experience;
+
+            Console.Write("Enter Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter Salary: ");
+            salary = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter Experience: ");
+            experience = int.Parse(Console.ReadLine());
+            if (salary < 0)
+            {
+                Console.WriteLine("Salary cannot be nagative");
+            }
+            else
+            {
+                if (experience < 2)
+                {
+                    bonus = salary * 0.05;
+                }
+                else if (experience <= 5)
+                {
+                    bonus = salary * 0.10;
+                }
+                else
+                {
+                    bonus = salary * 0.15;
+                }
+
+                finalSalary = salary + bonus;
+
+                Console.WriteLine("Employee: " + name);
+                Console.WriteLine("Bonus: " + bonus);
+                Console.WriteLine("Final Salary: " + finalSalary);
+            }
+        }
+
+    }
+}
