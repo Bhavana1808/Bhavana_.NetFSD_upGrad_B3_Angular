0591d20648d90f668c02e8f354c75f0d60f2dfb7
Bhavana1808 <yanalabhavana@gmail.com>
2026-03-12 17:46:09 +0530

    Add files via upload

---
+++ Week_4/Day_19/calculator_using_methods.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp3
+{
+    using System;
+
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int a;
+            int b;
+
+            Console.Write("Enter first number: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            b = int.Parse(Console.ReadLine());
+
+            Calculator c = new Calculator();
+
+            int addResult = c.Add(a, b);
+            int subResult = c.Subtract(a, b);
+
+            Console.WriteLine("Addition = " + addResult);
+            Console.WriteLine("Subtraction = " + subResult);
+        }
+    }
+}
