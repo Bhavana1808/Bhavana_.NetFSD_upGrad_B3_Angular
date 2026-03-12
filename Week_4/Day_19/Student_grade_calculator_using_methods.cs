c9c3e53b10de127ab50f763621d471613a21d04b
Bhavana1808 <yanalabhavana@gmail.com>
2026-03-12 17:47:31 +0530

    Add files via upload

---
+++ Week_4/Day_19/Student_grade_calculator_using_methods.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp4
+{
+
+    using System;
+
+    class Student
+    {
+        public double CalculateAverage(int m1, int m2, int m3)
+        {
+            double avg;
+            avg = (m1 + m2 + m3) / 3.0;
+            return avg;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int m1;
+            int m2;
+            int m3;
+
+            Console.Write("Enter mark 1: ");
+            m1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter mark 2: ");
+            m2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter mark 3: ");
+            m3 = int.Parse(Console.ReadLine());
+
+            Student s = new Student();
+
+            double average = s.CalculateAverage(m1, m2, m3);
+
+            string grade;
+
+            if (average >= 80)
+                grade = "A";
+            else if (average >= 60)
+                grade = "B";
+            else if (average >= 50)
+                grade = "C";
+            else
+                grade = "Fail";
+
+            Console.WriteLine("Average = " + average);
+            Console.WriteLine("Grade = " + grade);
+        }
+    }
+}
