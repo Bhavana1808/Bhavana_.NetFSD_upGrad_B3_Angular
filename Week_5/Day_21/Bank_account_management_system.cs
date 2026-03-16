f66c74027c7322ea25fe5037ca52f2cd3a30e8d7
Bhavana1808 <yanalabhavana@gmail.com>
2026-03-16 17:51:43 +0530

    Add files via upload

---
+++ Week_5/Day_21/Bank_account_management_system.cs
@@ -0,0 +1,68 @@
+﻿namespace ConsoleApp9 { 
+        
+class BankAccount
+        {
+            private int accountNumber;
+            private double balance;
+
+            public int AccountNumber
+            {
+                get { return accountNumber; }
+                set { accountNumber = value; }
+            }
+
+            public double Balance
+            {
+                get { return balance; }
+                private set { balance = value; }
+            }
+
+            public void Deposit(double amount)
+            {
+                if (amount > 0)
+                {
+                    balance += amount;
+                    Console.WriteLine("Deposit Successful");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Deposit Amount");
+                }
+            }
+
+            public void Withdraw(double amount)
+            {
+                if (amount > balance)
+                {
+                    Console.WriteLine("Insufficient Balance");
+                }
+                else
+                {
+                    balance -= amount;
+                    Console.WriteLine("Withdrawal Successful");
+                }
+            }
+
+            public void ShowBalance()
+            {
+                Console.WriteLine("Current Balance = " + balance);
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                BankAccount acc = new BankAccount();
+
+                acc.AccountNumber = 101;
+
+                acc.Deposit(5000);
+                acc.Withdraw(2000);
+
+                acc.ShowBalance();
+            }
+        }
+    }
+    
+
