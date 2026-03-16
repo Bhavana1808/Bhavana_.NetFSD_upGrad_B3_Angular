9b4063e85513214e77140cf20c026ef448a3fda1
Bhavana1808 <yanalabhavana@gmail.com>
2026-03-16 17:54:32 +0530

    Add files via upload

---
+++ Week_5/Day_21/vehicle_rental_system.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp15
+{
+    using System;
+
+    class Vehicle
+    {
+        public string Brand { get; set; }
+        public double RentalRatePerDay { get; set; }
+
+        public virtual double CalculateRental(int days)
+        {
+            return RentalRatePerDay * days;
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public override double CalculateRental(int days)
+        {
+            return (RentalRatePerDay * days) + 500;
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public override double CalculateRental(int days)
+        {
+            double total = RentalRatePerDay * days;
+            return total - (total * 0.05);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Vehicle v = new Car();
+
+            v.RentalRatePerDay = 2000;
+
+            Console.WriteLine("Total Rental = " + v.CalculateRental(3));
+        }
+    }
+}
