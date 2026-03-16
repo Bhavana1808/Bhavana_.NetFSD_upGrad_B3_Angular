26ad294dd3e5f604d1bee85d1513b9294d440642
Bhavana1808 <yanalabhavana@gmail.com>
2026-03-16 17:52:12 +0530

    Add files via upload

---
+++ Week_5/Day_21/salary_calculation(inheritance+polymorphism).cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp13
+{
+    using System;
+
+    class Employee
+    {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+
+        public virtual double CalculateSalary()
+        {
+            return BaseSalary;
+        }
+    }
+
+    class Manager : Employee
+    {
+        public override double CalculateSalary()
+        {
+            return BaseSalary + (BaseSalary * 0.20);
+        }
+    }
+
+    class Developer : Employee
+    {
+        public override double CalculateSalary()
+        {
+            return BaseSalary + (BaseSalary * 0.10);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee m = new Manager();
+            m.BaseSalary = 50000;
+
+            Employee d = new Developer();
+            d.BaseSalary = 50000;
+
+            Console.WriteLine("Manager Salary = " + m.CalculateSalary());
+            Console.WriteLine("Developer Salary = " + d.CalculateSalary());
+        }
+    }
+}
