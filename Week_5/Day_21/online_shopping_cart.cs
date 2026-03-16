4e6bdb55acda351badd6421fff9bbe524e2bc82f
Bhavana1808 <yanalabhavana@gmail.com>
2026-03-16 17:54:10 +0530

    Add files via upload

---
+++ Week_5/Day_21/online_shopping_cart.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp14
+{
+    using System;
+
+    class Product
+    {
+        private double price;
+
+        public string Name { get; set; }
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value > 0)
+                    price = value;
+                else
+                    Console.WriteLine("Invalid Price");
+            }
+        }
+
+        public virtual double CalculateDiscount()
+        {
+            return price;
+        }
+    }
+
+    class Electronics : Product
+    {
+        public override double CalculateDiscount()
+        {
+            return Price - (Price * 0.05);
+        }
+    }
+
+    class Clothing : Product
+    {
+        public override double CalculateDiscount()
+        {
+            return Price - (Price * 0.15);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Product p = new Electronics();
+
+            p.Price = 20000;
+
+            Console.WriteLine("Final Price after discount = " + p.CalculateDiscount());
+        }
+    }
+}
