47f5122b739ee9f10c9c2094eaa2f39c45d4a071
Bhavana1808 <yanalabhavana@gmail.com>
2026-03-23 18:17:21 +0530

    Add files via upload

---
+++ Week_6/Day_25/Application_Tracing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Trace.Listeners.Add(new TextWriterTraceListener("log.txt"));
+        Trace.AutoFlush = true;
+
+        Trace.WriteLine("Order Validation Started");
+        Trace.WriteLine("Order Validated");
+
+        Trace.TraceInformation("Processing Payment");
+        Trace.TraceInformation("Payment Successful");
+
+        Trace.WriteLine("Updating Inventory");
+        Trace.WriteLine("Inventory Updated");
+
+        Trace.WriteLine("Generating Invoice");
+        Trace.WriteLine("Invoice Generated");
+
+        Console.WriteLine("Order Process Completed. Check log.txt");
+    }
+}
---
+++ Week_6/Day_25/Async_FileLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Application Started...\n");
+
+        // Calling async logging multiple times
+        Task log1 = WriteLogAsync("User logged in");
+        Task log2 = WriteLogAsync("Product viewed");
+        Task log3 = WriteLogAsync("Order placed");
+
+        Console.WriteLine("Logs are being written asynchronously...\n");
+
+        // Wait for all logs to complete
+        await Task.WhenAll(log1, log2, log3);
+
+        Console.WriteLine("\nAll logs written successfully!");
+    }
+
+    // Async method to simulate file writing
+    static async Task WriteLogAsync(string message)
+    {
+        Console.WriteLine($"Start writing log: {message}");
+
+        // Simulate file I/O delay
+        await Task.Delay(2000);
+
+        Console.WriteLine($"Log written: {message}");
+    }
+}
