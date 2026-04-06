45a00c437d3675dd22c1666d92d79cfc0d073087
Bhavana1808 <yanalabhavana@gmail.com>
2026-04-06 18:26:15 +0530

    Add files via upload

---
+++ Week_8/Day_35/Repositories/IContactRepository.cs
@@ -0,0 +1,10 @@
+﻿public interface IContactRepository
+{
+    IEnumerable<ContactInfo> GetAllContacts();
+    IEnumerable<Company> GetCompanies();
+    IEnumerable<Department> GetDepartments();
+    ContactInfo GetContactById(int id);
+    void AddContact(ContactInfo contact);
+    void UpdateContact(ContactInfo contact);
+    void DeleteContact(int id);
+}
