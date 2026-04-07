9800e2716765659f4700e565cd557545206f299d
Bhavana1808 <yanalabhavana@gmail.com>
2026-04-07 17:43:22 +0530

    Add files via upload

---
+++ Week_8/Day_36/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class StudentController : Controller
+{
+    private readonly IStudentRepository _repo;
+
+    public StudentController(IStudentRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public IActionResult StudentsWithCourse()
+    {
+        var data = _repo.GetStudentsWithCourse();
+        return View(data);
+    }
+
+    public IActionResult CoursesWithStudents()
+    {
+        var data = _repo.GetCoursesWithStudents();
+        return View(data);
+    }
+
+    public IActionResult Create()
+    {
+        var courses = _repo.GetCourses();
+        ViewBag.Courses = courses;
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Student student)
+    {
+        _repo.AddStudent(student);
+        return RedirectToAction("StudentsWithCourse");
+    }
+
+    // EDIT (GET)
+    public IActionResult Edit(int id)
+    {
+        var student = _repo.GetStudentById(id);
+        ViewBag.Courses = _repo.GetCourses();
+        return View(student);
+    }
+
+    //  EDIT (POST)
+    [HttpPost]
+    public IActionResult Edit(Student student)
+    {
+        _repo.UpdateStudent(student);
+        return RedirectToAction("StudentsWithCourse");
+    }
+
+    //  DELETE
+    public IActionResult Delete(int id)
+    {
+        _repo.DeleteStudent(id);
+        return RedirectToAction("StudentsWithCourse");
+    }
+}
