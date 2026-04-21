d4b708a0cd6d970f0a98e571a3977b76772eadf3
Bhavana1808 <yanalabhavana@gmail.com>
2026-04-21 18:08:21 +0530

    Add files via upload

---
+++ Week_10/Day_46/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using ContactService.API.Models;
+using ContactService.API.Repository;
+using ContactService.API.Data;
+namespace ContactService.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddDbContext<ContactDbContext>(options =>
+     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        // Add services to the container.
+
+        builder.Services.AddScoped<IContactRepository, ContactRepository>();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+       
+
+        var app = builder.Build();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        // Configure the HTTP request pipeline.
+
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
---
+++ Week_10/Day_46/Repository/ContactRepository.cs
@@ -0,0 +1,74 @@
+﻿using ContactService.API.Data;
+using ContactService.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactService.API.Repository
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly ContactDbContext _context;
+
+        public ContactRepository(ContactDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Contact>> GetAll()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+
+        public async Task<Contact> GetContactById(int id)
+        {
+            var contact = await _context.Contacts.FirstOrDefaultAsync(c => c.ContactId == id);
+            if (contact == null)
+            {
+                throw new Exception("Contact not found");
+            }
+
+            return contact;
+        }
+
+        public async Task<Contact> Createcontact(Contact contact)
+        {
+            await _context.Contacts.AddAsync(contact);
+            await _context.SaveChangesAsync();
+
+            return contact;
+        }
+
+        public async Task<Contact> UpdateContact(int id, Contact contact)
+        {
+            var oldContact = await _context.Contacts.FirstOrDefaultAsync(c => c.ContactId == id);
+            if (oldContact == null)
+            {
+                throw new Exception("Contact not found");
+            }
+                ;
+            oldContact.Name = contact.Name;
+            oldContact.Email = contact.Email;
+            oldContact.Phone = contact.Phone;
+            oldContact.CategoryId = contact.CategoryId;
+
+            await _context.SaveChangesAsync();
+
+            return oldContact;
+        }
+
+        public async Task<bool> DeleteContact(int id)
+        {
+            var contact = await _context.Contacts.FirstOrDefaultAsync(c => c.ContactId == id);
+
+            if (contact == null)
+                return false;
+
+            _context.Contacts.Remove(contact);
+            await _context.SaveChangesAsync();
+
+            return true;
+
+        }
+
+    }
+}
+
