bacbff0d5c1106da0518dec78d8cb2a6af2ab06e
Bhavana1808 <yanalabhavana@gmail.com>
2026-04-21 18:09:16 +0530

    Add files via upload

---
+++ Week_10/Day_45/Repository/IContactRepository.cs
@@ -0,0 +1,18 @@
+﻿using ContactService.API.Models;
+
+namespace ContactService.API.Repository
+{
+    public interface IContactRepository
+    {
+        Task<List<Contact>> GetAll();
+
+        Task<Contact> GetContactById(int id);
+
+        Task<Contact> Createcontact(Contact contact);
+
+        Task<Contact> UpdateContact(int id, Contact contact);
+
+        Task<bool> DeleteContact(int id);
+    }
+}
+
